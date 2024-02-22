f0de43d3d1847929daf73f7c3146c784d399278a
xHurricane <molham.work9@gmail.com>
2024-02-22 23:22:13 +0200

    Added All

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express = require('express');
+const app = express();
+const axios = require('axios')
+
+app.post('/update', async (req, res) => {
+
+    var response = await axios.get('https://atccontests.glitch.me/updatecontests');
+
+    console.log("response");
+
+    return res.status(200).send();
+});
+
+async function keepPinging() {
+
+    var url = "https://atccontests.glitch.me/";
+
+    var response = await axios.get("https://atccontests.glitch.me/");
+
+    console.log("Sent Ping to ATC");
+
+    setTimeout(keepPinging, 1000 * 60 * 5);
+
+}
+
+app.listen(process.env.PORT || 3000, async (err) => {
+
+    console.log("Running on Port 30000!!!");
+
+    await keepPinging();
+    
+
 })
-app.listen(process.env.PORT || 3000)
